83d3cd500ad547eec7edd07d2ca1a380729fd2c1
Kike0013 <kique0013@hotmail.com>
2023-03-29 20:31:39 -0400

    [add] Exact and definition searches

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Letters from './components/Letters'
 import Navbar from './components/Navbar'
 import ResultsContainer from './components/ResultsContainer'
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import { loadDictionary } from './core/services/netService/dictionaryService'
 import './styles/App.css'
 import { Pagination } from './components/Pagination'
@@ -11,13 +11,23 @@ function App() {
 
   const [data, setData] = useState([])
   const [currentData, setCurrentData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, setIsLoading] = useState(true)
   const [notFound, setNotFound] = useState(false)
   const [page, setPage] = useState(0)
 
   useEffect(() => {
     loadDictionary()
-      .then(({ data }) => setData(data.sort()))
+      .then(({ data }) => setData(data.sort((a, b) => {
+        const wordA = a.word.toLowerCase();
+        const wordB = b.word.toLowerCase();
+        if (wordA < wordB) {
+          return -1;
+        }
+        if (wordA > wordB) {
+          return 1;
+        }
+        return 0;
+      })))
       .catch(error => console.log(error))
   }, [])
 
@@ -28,9 +38,17 @@ function App() {
     notFound && setNotFound(false)
   }
 
-  const handleSearch = (searchValue) => {
-    let arr = data.filter(el => el.word.toLowerCase().includes(searchValue.current.value.toLowerCase()))
-    setPage(0)
+  const handleSearch = (searchValue, isExact, isDef, event) => {
+    event && event.preventDefault();
+    if (!searchValue.current.value) return null
+
+    let arr;
+
+    isExact
+      ? arr = data.filter(el => el.word.toLowerCase() === searchValue.current.value.toLowerCase())
+      : isDef
+        ? arr = data.filter(el => el.word.toLowerCase().includes(searchValue.current.value.toLowerCase()) || el.definition.toLowerCase().includes(searchValue.current.value.toLowerCase()))
+        : arr = data.filter(el => el.word.toLowerCase().includes(searchValue.current.value.toLowerCase()))
 
     if (arr.length) {
       setCurrentData(arr)
@@ -39,15 +57,16 @@ function App() {
       !notFound && setNotFound(true)
     }
 
-    console.log(arr);
-    console.log(searchValue.current.value);
+    setPage(0)
   }
 
-  const prevPage = () => !page ? null : setPage(prevPage => prevPage - 1)
+  const prevPage = () => !page
+    ? null
+    : setPage(prevPage => prevPage - 1)
 
-  const nextPage = () => {
-    currentData.length - (page * 10) <= 10 ? null : setPage(prevPage => prevPage + 1)
-  }
+  const nextPage = () => currentData.length - (page * 10) <= 10
+    ? null
+    : setPage(prevPage => prevPage + 1)
 
   return (
     <div className="App">
@@ -56,14 +75,22 @@ function App() {
       {!notFound ? <ResultsContainer currentData={currentData} page={page} /> : <NotFound />}
       <Pagination nextPage={nextPage} prevPage={prevPage} />
       {`currentData.length: ${currentData.length}`}
+      {/* For testig: */}
       <br />
       {`Page: ${page}`}
       <br />
       {currentData.length - (page * 10)}
       <br />
       {currentData.length % 10}
+      <br />
     </div>
   )
 }
 
 export default App
+
+/* to do */
+//Add searching: value
+//return Welcome if empty
+//fix isLetter
+//Loader
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,38 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = ({ handleSearch }) => {
 
   const searchValue = useRef();
+  const exactValue = useRef();
+  const defValue = useRef();
 
   return (
     <div>
       <h2>Logo</h2>
       <div>
-        {/* <form onSubmit={handleSearch(searchValue)}> */}
-        <form>
+        <form onSubmit={(e) => handleSearch(searchValue, exactValue.current.checked, defValue.current.checked, e)}>
           <input ref={searchValue} type='text' name='input-search' id='input-search'></input>
-          <button type='button' onClick={() => handleSearch(searchValue)}>Search</button>
-          <input type='checkbox' name='match-search' id='match-search'></input>
-          <label htmlFor='match-search'>Match exact search</label>
-          <input type='checkbox' name='include-definition' id='include-definition'></input>
-          <label htmlFor='include-definition'>Include definitions</label>
+          <button type='button' onClick={(e) => {
+            handleSearch(searchValue, exactValue.current.checked, defValue.current.checked, e)
+          }}>
+            Search
+          </button>
+          <input ref={exactValue} type='checkbox' name='match-search' id='match-search'
+            onChange={(e) => {
+              defValue.current.checked = false;
+              handleSearch(searchValue, e.target.checked, defValue.current.checked)
+            }}
+          >
+          </input>
+          <label htmlFor='match-search'>Search exact word</label>
+          <input ref={defValue} type='checkbox' name='include-definition' id='include-definition'
+            onChange={(e) => {
+              exactValue.current.checked = false;
+              handleSearch(searchValue, exactValue.current.checked, e.target.checked)
+            }}
+          >
+          </input>
+          <label htmlFor='include-definition'>Search in definitions</label>
         </form>
       </div>
       <div>
