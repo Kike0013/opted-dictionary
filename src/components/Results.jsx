18d86ea38beb3d2cbd6bd2d405a9a5e6184cd503
Kike0013 <kique0013@hotmail.com>
2023-05-05 19:51:40 -0400

    [add] Results Modal Window

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FixedSizeList as List } from 'react-window'
 import Home from './Home';
+import Modal from './Modal';
 
 const Results = ({ currentData, page }) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [modalIndex, setModalIndex] = useState(0)
+
+    const handleModalData = e => {
+        setModalIndex(Number(e.target.id))
+        setIsOpen(true)
+    }
 
     const result = ({ index, style }) => (
         <div style={style} className='result-card'>
             <div>
                 {/* <div style={{display: 'flex'}}> */}
-                <h3 className='word'>
+                <h3 id={index} className='word' onClick={handleModalData}>
                     {currentData[index + (page * 10)].word}
                 </h3>
                 {/* <p>{currentData[index + (page * 10)].type}</p> */}
@@ -35,6 +44,10 @@ const Results = ({ currentData, page }) => {
                 >
                     {result}
                 </List>}
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <h3>{currentData[(modalIndex + (page * 10))]?.word}</h3>
+                <h3>{currentData[modalIndex + (page * 10)]?.definition}</h3>
+            </Modal>
         </>
     )
 }
