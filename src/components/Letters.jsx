e2ac409a3b8aaeb54d61e532c702c93af1ca6f09
Kike0013 <kique0013@hotmail.com>
2023-05-09 22:26:44 -0400

    [add] Active-Selected Letter

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import '../styles/Letters.css'
 
-const Letters = ({ searchByLetter }) => {
+const Letters = ({ searchByLetter, isSelected }) => {
 
     const AZ = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     const letters = AZ.map((letter) => { return { label: letter, value: letter } })
 
     return (
         <div id='letters-container'>
-            {letters.map((el, index) => {
-                return <div className='letter' key={index} onClick={() => searchByLetter(el.label)}>
-                    <p>{el.label}</p>
+            {letters.map((el, i) => {
+                return <div className={`letter${isSelected === el.label ? ' active' : ''}`} key={i} onClick={() => searchByLetter(el.label)}>
+                    <p><b>{el.label}</b></p>
                 </div>
             })}</div>
     )
